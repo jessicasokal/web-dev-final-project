6bf80b6c9e776d879873c86e7ce22667e324ef62
yomal148 <amaltharyan@gmail.com>
2022-04-11 20:32:19 -0400

    creating movies and favorites list for homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import unirest from 'unirest';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import HomePage from './pages/home-page';
 
 function App() {
   return (
+      <BrowserRouter>
     <div className="App">
       <header className="App-header">
-
+          <Routes>
+              <Route path="/" element={<HomePage/>}/>
+          </Routes>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home-page.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+import MovieList from '../components/home/movie-list';
+import MovieListHeading from '../components/home/movie-list-heading';
+import SearchBox from '../components/home/search-box';
+import AddFavourites from '../components/home/add-favs';
+import RemoveFavourites from '../components/home/remove-favs';
+
+const HomePage = () => {
+    const [movies, setMovies] = useState([]);
+    const [favourites, setFavourites] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=7ba0a1dd`;
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    };
+
+    useEffect(() => {
+        getMovieRequest(searchValue);
+    }, [searchValue]);
+
+    useEffect(() => {
+        const movieFavourites = JSON.parse(
+            localStorage.getItem('react-movie-app-favourites')
+        );
+
+        if (movieFavourites) {
+            setFavourites(movieFavourites);
+        }
+    }, []);
+
+    const saveToLocalStorage = (items) => {
+        localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+    };
+
+    const addFavouriteMovie = (movie) => {
+        const newFavouriteList = [...favourites, movie];
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    };
+
+    const removeFavouriteMovie = (movie) => {
+        const newFavouriteList = favourites.filter(
+            (favourite) => favourite.imdbID !== movie.imdbID
+        );
+
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    };
+
+    return (
+        <div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Movies' />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
+            <div className='row'>
+                <MovieList
+                    movies={movies}
+                    handleFavouritesClick={addFavouriteMovie}
+                    favouriteComponent={AddFavourites}
+                />
+            </div>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Favourites' />
+            </div>
+            <div className='row'>
+                <MovieList
+                    movies={favourites}
+                    handleFavouritesClick={removeFavouriteMovie}
+                    favouriteComponent={RemoveFavourites}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
