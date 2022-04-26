4303694ac25e14ab67ebef7fdf9d91a8f16d3a8d
jessicasokal <sokal.j@northeastern.edu>
2022-04-26 18:09:00 -0400

    adding work

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import './profile.css';
 
 const api = axios.create({
     withCredentials: true
@@ -9,6 +10,7 @@ const api = axios.create({
 const Profile = () => {
     const [currentUser, setCurrentUser] = useState({})
     const navigate = useNavigate()
+    const emailRef = useRef();
     const fetchCurrentUser = async () => {
         try {
             const response = await api.post("http://localhost:4000/api/profile")
@@ -17,41 +19,84 @@ const Profile = () => {
             navigate('/')
         }
     }
+    const editEmail = async (_id, user) => {
+        try {
+            await api.put(`http://localhost:4000/api/users/${_id}`, user)
+        } catch (e) {
+            console.log(e)
+        }
+        navigate('/signin')
+    }
     useEffect(() => {
         fetchCurrentUser()
     }, [])
-/*
-    const handleUpdateBtn = async () => {
-        try {
-            if (currentUser) {
-                const response = await api.put(`http://localhost:4000/api/${currentUser._id}`)
-            }
-            setCurrentUser(response.data)
-        } catch (e) {
-            navigate('/')
-        }
-    }
 
- */
     return (
         <div>
             <h1>My Profile</h1>
-            <div>
-                <h4>Username: {currentUser.username}</h4>
-                <h4>Password: {currentUser.password}</h4>
+            <div className={'row'}>
+                <div className={'col-10'}>
+                    <div className={'row'}>
+                    <span className={'wd_username'}>@{currentUser.username}
+                        <button className={'btn btn-primary rounded-pill wd_small ms-4'}>Follow</button>
+                    </span>
+                    </div>
 
+                    <div className={'row mt-3'}>
+                        <div>
+                            <span className={'wd_bold'}>
+                            Email:
+                            </span>
+                            {currentUser.email}
+
+                        <input
+                                ref={emailRef}
+                                placeholder="email"
+                                type="email"
+                                className={'ms-3'}
+                        />
+                        <button
+                            className={'btn btn-primary rounded-pill wd_small ms-4'}
+                            onClick={() => editEmail(currentUser._id, {
+                                ...currentUser,
+                                email: emailRef
+                            })}>
+                            Edit
+                        </button>
+
+                            <span className={'ps-5'}>
+                                <span className={'wd_bold'}>Password:</span>
+                                {currentUser.password}
+                            </span>
+
+                        <button className={'btn btn-primary rounded-pill wd_small ms-4'}>
+                            Edit
+                        </button>
+
+                        </div>
+
+                    </div>
+                    <div className={'row'}>
+                        <div className={'pt-5 wd_sidebar'}>Liked Movies</div>
+                        {currentUser.likedMovies}
+                    </div>
+                </div>
+                <div className={'col-2'}>
+                    <div className={'wd_sidebar'}>Following</div>
+                    {currentUser.followers}
+                    <div className={'wd_sidebar'}>Followers</div>
+                    {currentUser.following}
+                </div>
+                {JSON.stringify(currentUser)}
             </div>
-            {JSON.stringify(currentUser)}
+        <br/>
+            <br/>
+            <br/>
+            <br/>
+
         </div>
     );
 };
 
 export default Profile;
 
-/*
-                <button
-                    onClick={handleUpdateBtn}
-                    className="btn btn-primary">
-                    Update Information
-                </button>
-                */
