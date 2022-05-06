a318c1d720377ee9f7f32f2c1d65f0e7ab58230f
jessicasokal <sokal.j@northeastern.edu>
2022-05-06 11:25:15 -0400

    change details

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import {useLocation} from "react-router-dom";
 import './index.css';
 import Movie from "./movie";
+import {getUser} from "../../services/user-service";
+import {fetchMovieByIMDBID} from "../../services/movie-service";
 
 
 const Details = () => {
@@ -9,6 +11,7 @@ const Details = () => {
     const textArray = location.split('/');
     const movieID = textArray[2];
     const [movie, setMovie] = useState([])
+    const [ourMovie, setOurMovie] = useState([])
 
     const url =
         `https://api.themoviedb.org/3/movie/${movieID}?api_key=9e019a5736bc48ae537fdcff22fd8a1e`;
@@ -20,30 +23,36 @@ const Details = () => {
     const fetchMovie = async () => {
         const data = await fetch(url);
         const movie = await data.json();
+        console.log(`MOVIE: ${JSON.stringify(movie)}`)
         setMovie(movie)
     };
 
+    // get the movie corresponding to the paths ID
+    const fetchOurMovie = async () => {
+        const selectedMovie = await fetchMovieByIMDBID(movieID)
+        setOurMovie(selectedMovie)
+    }
+
+    useEffect(() => {
+        fetchOurMovie()
+    }, [])
 
     return (
-        <div className={'row'}>
-            <div className="movie-details">
-                <Movie key={movie.id} movie={movie} />
+        <div className={'row mt-4'}>
+            <div className={'col-5'}>
+                <img src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt={movie.path} />
             </div>
-            <table>
-                <tr>
-                    <th>Movie Details</th>
-                </tr>
-                <tr>
-                    Overview: {JSON.stringify(movie.overview)}
-                </tr>
-                <tr>
-                    Release Date: {JSON.stringify(movie.release_date)}
-                </tr>
-                <tr>
-                    Tagline: {JSON.stringify(movie.tagline)}
-                </tr>
+            <div className={'col-7 ps-2'}>
+                <h1>{movie.title}</h1>
+                <div className={'m-2'}>{movie.overview}</div>
+                <h5 className={'m-2'}>Release date: {movie.release_date}</h5>
+                <h5 className={'m-2'}>Likes: {ourMovie.likes}</h5>
 
-            </table>
+            </div>
+            <h3>Comments</h3>
+
+
+
 
         </div>
 
